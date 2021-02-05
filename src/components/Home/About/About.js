9bb05ef0af4d80f506fdbc4e89ec8f1376266148
Jorge <jhessle04@gmail.com>
2021-02-05 19:38:55 -0300

    U

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-//import './Hero.css'
+import './About.css'
 import { Link } from 'react-router-dom'
  
 
@@ -9,61 +9,51 @@ const About = () => {
 
 
 
-<section id="about-us" class="container">
-        <div class="row">
-            <div class="col-sm-6">
-                <h2>Quienes somos</h2>
-                <p>Rescatamos el arte de las antiguas barberías y la experiencia que en ellas se vivía.
-Conjuntamos tradiciones, servicios de altos estándares de calidad e higiene, y los mejores productos de grooming y shaving.
-Aquí encontrarás ese espacio que hemos perdido los hombres, donde cortarse el pelo y afeitarse se convierten en una terapia de amigos y relajación, más que en un hábito.
-Sillas de barbero, espuma, toalla caliente y navaja libre… regresa el ritual del hombre.</p>
-            </div> 
-            <div class="col-sm-6">
-                <h2></h2>
-
-                
-                <div>
-                 <img class="img-responsive img-thumbnail" src="images/blog/member1.jpg" alt="" /> 
-
-                </div>
 
 
-            </div> 
-        </div> 
+<div class="page-about u-s-p-t-80 mt-5">
+		<div class="container">
+				<div class="row">
+						<div class="col-lg-6 col-md-12">
+								<div class="about-me-info u-s-m-b-30">
+										<h1>Bienvenidos a
+												<span>Barbería Parodi</span>
+										</h1>
+										<p>El arte de hacer cerveza es un proceso que lleva tiempo y mucho cuidado, controlando siempre la higiente del equipo y la propia, se necesita conocimientos y experiencia. 
+											Habiendo hecho una tecnicatura en alimentos con especialización en cerveza durante el período 2004-2008 en Suiza, y haber recorrido cada instancia de la producción de 
+											cerveza, desde la plantación y adquisición de. materias primas hasta la venta de la cerveza en negocios, bares y restaurantes, después de tanto tiempo nace este proyecto 
+											cervecero el 16 de septiembredel 2020 como negocio familiar.
+										</p>
 
-        <div class="gap"></div>
-        <h1 class="center">Conocé a nuestro equipo</h1>
-        <p class="lead center">Meet our high skilled team and also the founder of the company.</p>
-        <div class="gap"></div>
+										
 
-        <div id="meet-the-team" class="row" >
- 
+										<p> Utilizando como locación la casa de mis abuelos y haciendo uso de alguna de sus herramientas, en homenaje a ellos nombramos la cervecer+ia "Grossi", nombre que significa 
+											"Abuelos" en suizo alemán. A los maternos, gracias a Willhelm, gracias a Herta y a los paternos, gracias a Josef Marie, gracias Julia.
+										</p>
+										
 
-            <div class="col-md-4 col-xs-6">
-                <div class="center">
-                    <p><img class="img-responsive img-thumbnail img-circle" src="images/blog/member2.jpg" alt="" /></p>
-                    <h5>Julian V. Gomez<small class="designation muted">Especialista en cortes de pelo</small></h5>
-                    <p>  </p>
-                </div>
-            </div>        
-            <div class="col-md-4 col-xs-6">
-                <div class="center">
-                    <p><img class="img-responsive img-thumbnail img-circle" src="images/blog/founder.png" alt="" /></p>
-                    <h5>Juan. Alvez<small class="designation muted">Dueño</small></h5>
-                    <p> </p>
-                </div>
-            </div>        
-            <div class="col-md-4 col-xs-6">
-                <div class="center">
-                    <p><img class="img-responsive img-thumbnail img-circle" src="images/blog/member3.jpg" alt="" /></p>
-                    <h5>Carlos J. Martinez<small class="designation muted">Estilista de bigotes y barba.</small></h5>
-                    <p> </p>
-                </div>
-            </div>
-        </div> 
-    </section> 
-
-
+										<p>Desde ya agradezco a todas las personas que hicieron esto posible y a los que siguen aportando apoyo de cualquier manera. Ojalá sea este un proyecto exitoso y sino, de 
+											cualquier manera, seguimos disfrutando del proceso aprendiendo de él, conociendo gente nueva y pasando tiempo con seres queridos. 
+										</p>
+										
+										
+										<p>Atte. El equipo de Grossi (somos pocos, 
+												pero hay equipo!)</p>
+								</div>
+						</div>
+						<div class="col-lg-6 col-md-12">
+								<div class="about-me-image u-s-m-b-30">
+										<div class="banner-hover effect-border-scaling-gray">
+												<img class="img-fluid" src="images/about.jpg" alt="About Picture"/>
+										</div>
+								</div>
+								<div class="about-social text-center u-s-m-b-30">
+									
+								</div>
+						</div>
+				</div>
+		</div>
+</div>
 
 
 
