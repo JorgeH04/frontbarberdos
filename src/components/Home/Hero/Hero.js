dfb1eff3b29a1b7a26511289b06a7324ff91732d
Jorge <jhessle04@gmail.com>
2021-02-26 17:57:45 -0300

    J

--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -10,11 +10,7 @@ const Hero = () => {
         
         <section id="main-slider" class="no-margin">
         <div class="carousel slide wet-asphalt">
-            <ol class="carousel-indicators">
-                <li data-target="#main-slider" data-slide-to="0" class="active"></li>
-                <li data-target="#main-slider" data-slide-to="1"></li>
-                <li data-target="#main-slider" data-slide-to="2"></li>
-            </ol>
+         
             <div class="carousel-inner">
                 <div class="item active" >
                     <div class="container">
@@ -39,12 +35,7 @@ const Hero = () => {
                 
             </div>
         </div>
-        <a class="prev hidden-xs" href="#main-slider" data-slide="prev">
-            <i class="icon-angle-left"></i>
-        </a>
-        <a class="next hidden-xs" href="#main-slider" data-slide="next">
-            <i class="icon-angle-right"></i>
-        </a>
+     
     </section>
 
 
